e1b4ad41f5894761e3511f4de7615db4fb0fd1e1
Jacob <32347257+SysBreak256@users.noreply.github.com>
2019-04-23 10:13:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pygame, random
+screenWidth = 800
+screenHeight = 400
+FPS = 60
 
+def setup():
+    global screenHeight, screenWidth, FPS, screen
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
+    pygame.display.set_caption("Wizard Tower")
+    clock = pygame.time.Clock()
+    main()
 
 def main():
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome to Wizard Tower!")
+    global screenWidth, screenHeight, screen
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
 
-main()
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome to the Wizard Tower!")
+setup()
+pygame.QUIT
