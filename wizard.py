59dd1dd6fe83d9e450a5eecdbcfecdbb875c939d
Jacob <32347257+SysBreak256@users.noreply.github.com>
2019-05-09 09:38:42 -0400

    0.2.4

---
+++ wizard.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+def main():
+
+    image = pygame.Surface((50,50))
+    image.fill((36, 76, 0))
+
+    background = pygame.Surface(screen.get_size())
+    background.fill((255, 255, 255))
+    screen.blit(background, (0,0))
+    screen.blit(image, (300,300))
+
+    clock = pygame.time.Clock()
+    keepGoing = True
+    pygame.mouse.set_visible(True)
+
+    while keepGoing:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+        pygame.display.flip()
+if __name__ == "__main__":
+    main()
